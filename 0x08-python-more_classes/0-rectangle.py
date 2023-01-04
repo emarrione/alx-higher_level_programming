cf100af4b136c760e4d18ead33bcb1a720da1cce
emarrione <emarrione@gmail.com>
2023-01-04 12:14:13 +0000

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-"""
+# 0-rectangle.py
 
-Defines an empty class Rectangle
+# Brennan D Baraban <375@holbertonschool.com>
 
-"""
+"""Defines a Rectangle class."""
 
 
 
@@ -12,6 +12,6 @@ Defines an empty class Rectangle
 
 class Rectangle:
 
-        """Empty representation of a rectangle"""
+            """Represent a rectangle."""
 
-            pass
+                pass
